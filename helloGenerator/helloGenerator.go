1f70ba1b0443118189b2a785a6a37efc43175a1e
Piotr Pieróg <ppierog@gmail.com>
2024-06-21 12:51:36 +0200

    Implement HelloMsg

--- helloGenerator/helloGenerator.go
+++ helloGenerator/helloGenerator.go
@@ -1,25 +1,25 @@
-package authMsgGenerator
+package helloGenerator
 
 import (
 	"crypto/sha256"
 	"time"
 )
 
-type AuthMsgGenerator struct {
+type HelloGenerator struct {
 }
 
-type AuthMsg struct {
-	Msg       string
-	Sha256    []byte
-	IssuedAt  int64
-	ExpiresAt int64
+type HelloMsg struct {
+	Msg       string `json:"msg,omitempty"`
+	Sha256    []byte `json:"sha_256,omitempty"`
+	IssuedAt  int64  `json:"issued_at,omitempty"`
+	ExpiresAt int64  `json:"expires_at,omitempty"`
 }
 
-func Init() AuthMsgGenerator {
-	return AuthMsgGenerator{}
+func Init() HelloGenerator {
+	return HelloGenerator{}
 }
 
-func (s AuthMsgGenerator) Get() AuthMsg {
+func (s HelloGenerator) Get() HelloMsg {
 	msgs := []string{
 		"I met a traveller from an antique land",
 		"Who said: Two vast and trunkless legs of stone",
@@ -39,10 +39,10 @@ func (s AuthMsgGenerator) Get() AuthMsg {
 	}
 
 	h := sha256.New()
-	h.Write([]byte(msgs[10]))
+	h.Write([]byte(msgs[9]))
 	bs := h.Sum(nil)
 
-	return AuthMsg{Msg: msgs[10], Sha256: bs, IssuedAt: time.Now().Unix(),
+	return HelloMsg{Msg: msgs[9], Sha256: bs, IssuedAt: time.Now().Unix(),
 		ExpiresAt: time.Now().Add(time.Minute * 15).Unix()}
 
 }
